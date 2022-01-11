92eef3759b9e4d384fc44f0512d9de4717522b4a
koap <37614244+KoapT@users.noreply.github.com>
2022-01-11 15:40:01 +0800

    添加软链接读取

--- walk遍历路径.py
+++ walk遍历路径.py
@@ -3,7 +3,7 @@
 
 def walk(dir_path):
     import os
-    for (root, dirs, files) in os.walk(dir_path):
+    for (root, dirs, files) in os.walk(dir_path, followlinks=True):  # 要读取软链接时，添加followlinks=True
         for file in files:
             print(os.path.join(root, file))
 
